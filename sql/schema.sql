4bab55685d3302387e300f46d3b4323084e5a44e
Olaf Steenbergen <steenbergenolaf@gmail.com>
2022-09-29 16:13:56 +0200

    ja

---
+++ sql/schema.sql
@@ -0,0 +1,3 @@
+CREATE table users(id INTEGER PRIMARY KEY, empid INTEGER, name text not null, password text not null, admin boolean not null DEFAULT '0', creation_date timestamp DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(empid) REFERENCES emp(empid));
+CREATE table emp(empid INTEGER PRIMARY KEY, name text not null, email text, phone integer, address text, joining_date timestamp DEFAULT CURRENT_TIMESTAMP, total_proects integer default 1, total_test_case integer DEFAULT 1, total_defects_found integer DEFAULT 1, total_defects_pending integer DEFAULT 1);
+CREATE table projects(projectid INTEGER PRIMARY KEY, empres INTEGER, name text not null, description text not null, location text not null, FOREIGN KEY(empres) REFERENCES emp(empid));
